768f3be635235c4a730ce70a8f1a2e0301366fc0
jeffjeffjeffh <jeffjeffjeffh@yahoo.com>
2023-11-28 10:27:52 -0800

    server running

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	PORT := "8080"
+
+	mux := http.NewServeMux()
+	corsMux := corsWrapper(mux)
+
+	server := &http.Server{
+		Addr: ":" + PORT,
+		Handler: corsMux,
+	}
+
+	log.Printf("Server listening on port: " + PORT)
+	log.Fatal(server.ListenAndServe())
+}
+
+func corsWrapper(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
