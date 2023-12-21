50612fcd21974228900e8bf2a3c50dcc13064200
Jeff Harm <jeffjeffjeffh@yahoo.com>
2023-12-21 11:57:17 -0800

    added api key check for updating subscribed status

--- handler_webhooks.go
+++ handler_webhooks.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerWebhooks(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	authStrings := strings.Split(authHeader, " ")
+
+	if authHeader == "" || len(authStrings) != 2 {
+		err := errors.New("missing or malformed authorization")
+		log.Println(err)
+		writeError(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	authString := authStrings[1]
+
+	if authString != os.Getenv("POLKA_API_KEY") {
+		err := errors.New("polka api key mismatch")
+		log.Println(err)
+		writeError(w, err, http.StatusUnauthorized)
+		return
+	}
+
 	hook, err := decodeWebhooksParams(r)
 	if err != nil {
 		writeError(w, err, http.StatusBadRequest)
